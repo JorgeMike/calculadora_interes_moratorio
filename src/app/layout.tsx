fc08c895824b33043b59030996b6c079e9c5c50f
jorgemike <alvaradojr2001@gmail.com>
2025-03-16 18:11:58 -0500

    creditos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,20 +30,31 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${roboto.className}`} data-bs-theme="dark">
         {children}
-        <footer className="text-center text-muted mt-5 p-3 border-top small">
-          <p>
-            La información proporcionada por esta calculadora es únicamente de
-            carácter informativo y no sustituye el asesoramiento legal o
-            financiero.
-          </p>
-          <p>
-            Los valores de <strong>UDIS</strong> y <strong>CCP-UDIS</strong>{" "}
-            están disponibles hasta el
-            <strong> 25 de marzo de 2025</strong>, mientras que los datos de{" "}
-            <strong>CCP-UDIS</strong>
-            solo cubren hasta <strong>febrero de 2025</strong>.
-          </p>
-        </footer>
+        <footer className="text-center text-muted mt-5 p-3 border-top">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-9">
+        <p>
+          La información proporcionada por esta calculadora es únicamente de carácter informativo y no sustituye el asesoramiento legal o financiero.
+        </p>
+        <p>
+          Los valores de <strong>UDIS</strong> y <strong>CCP-UDIS</strong> están disponibles hasta el <strong>25 de marzo de 2025</strong>, mientras que los datos de <strong>CCP-UDIS</strong> solo cubren hasta <strong>febrero de 2025</strong>.
+        </p>
+      </div>
+      <div className="col-md-3 text-md-end border-start">
+        <p className="small">
+          Desarrollado por:<br />
+          <strong>Alvarado Reyes Jorge Miguel</strong>, <br />
+          <strong>Monroy Alarcon Omar Ulises</strong>.
+          <strong>Tirado Luna Jose Angel</strong>, <br />
+          <strong>Jimenez Pineda Leydi Montserrat</strong> y <br />
+        </p>
+        <p>© {new Date().getFullYear()}</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
       </body>
     </html>
   );
